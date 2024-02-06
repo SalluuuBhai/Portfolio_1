599cb7801167a9c868bdf0107ddada99a6974b14
SalluuuBhai <md.salmanfaris03@gmail.com>
2024-02-06 20:15:38 +0530

    form updated 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,18 +28,61 @@ function linkAction() {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 //CONTACT SEND MAIL
-function sendEmail() {
-  Email.send({
-    Host: "smtp.gmail.com",
-    Username: "md.salmanfaris03@gmail.com",
-    Password: "fgbuthokduglrqse",
-    To: "md.salmanfaris03@gmail.com",
-    From: document.getElementById("email").value,
-    Subject: "New Contact From Enquiry",
-    Body: "Name : " + document.getElementById("name").value
-     + "<br> Email : " + document.getElementById("email").value
-     + "<br> Message : " + document.getElementById("message").value
-  }).then(
-      message => alert("Message Send Succesfully")
-      );
-}
+
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function(e) {
+    const formData = new FormData(form);
+    e.preventDefault();
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
+
+
+// function sendEmail() {
+//   Email.send({
+//     Host: "smtp.gmail.com",
+//     Username: "md.salmanfaris03@gmail.com",
+//     Password: "fgbuthokduglrqse",
+//     To: "md.salmanfaris03@gmail.com",
+//     From: document.getElementById("email").value,
+//     Subject: "New Contact From Enquiry",
+//     Body: "Name : " + document.getElementById("name").value
+//      + "<br> Email : " + document.getElementById("email").value
+//      + "<br> Message : " + document.getElementById("message").value
+//   }).then(
+//       message => alert("Message Send Succesfully")
+//       );
+// }
